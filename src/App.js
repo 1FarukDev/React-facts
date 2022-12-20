b08adf293e66be4c74fe6b5b096c04fdc50d8d4f
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2022-12-20 22:56:08 +0100

    react props with jokes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import './App.css';
+import Jokes from './components/jokes';
 
 export default function App() {
   return (
    <div className='container'>
      <Navbar />
      <Main />
+     <Jokes />
    </div>
    )
 } 
